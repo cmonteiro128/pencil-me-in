1c27f8a778a1a994c278ec6b42cc0bc498e8bfb6
Kenzo Wong <wongk8@wit.edu>
2018-02-19 21:44:54 -0500

    Added main UI login screen and partial button functionality

--- app/src/main/java/mobileapp/wit/edu/pencilmein/MainActivity.java
+++ app/src/main/java/mobileapp/wit/edu/pencilmein/MainActivity.java
@@ -2,6 +2,9 @@ package mobileapp.wit.edu.pencilmein;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.buttonLogin);
+
+        final EditText usernameEditText = (EditText) findViewById(R.id.editTextUsername);
+        final EditText passwordEditText = (EditText) findViewById(R.id.editTextPassword);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+
+            }
+        });
     }
 }
