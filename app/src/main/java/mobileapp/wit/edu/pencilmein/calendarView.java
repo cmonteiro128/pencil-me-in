e6c28c73a056827a3c7ab028bc9911d333f79607
topaliana <topaliana@wit.edu>
2018-03-19 09:43:58 -0400

    Added Calendar UI

---
+++ app/src/main/java/mobileapp/wit/edu/pencilmein/calendarView.java
@@ -0,0 +1,27 @@
+package mobileapp.wit.edu.pencilmein;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.alamkanak.weekview.WeekView;
+
+public class calendarView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar_view);
+        // Get a reference for the week view in the layout.
+        mWeekView = (WeekView) findViewById(R.id.weekView);
+
+// Set an action when any event is clicked.
+        mWeekView.setOnEventClickListener(mEventClickListener);
+
+// The week view has infinite scrolling horizontally. We have to provide the events of a
+// month every time the month changes on the week view.
+        mWeekView.setMonthChangeListener(mMonthChangeListener);
+
+// Set long press listener for events.
+        mWeekView.setEventLongPressListener(mEventLongPressListener);
+    }
+}
