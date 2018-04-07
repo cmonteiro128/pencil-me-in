4181771595fe4c2c9b975933fe7803efe8aab212
Chris Monteiro <chris@viperapps.net>
2018-04-07 17:24:31 -0400

    Add NotificationPublisher class and changes to task view layout

--- app/src/main/java/mobileapp/wit/edu/pencilmein/NotificationPublisher.java
+++ app/src/main/java/mobileapp/wit/edu/pencilmein/NotificationPublisher.java
@@ -4,11 +4,13 @@ package mobileapp.wit.edu.pencilmein;
  * Created by chris on 4/3/18.
  */
 
+import android.app.NotificationChannel;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 
 public class NotificationPublisher extends BroadcastReceiver {
 
@@ -21,6 +23,14 @@ public class NotificationPublisher extends BroadcastReceiver {
 
         Notification notification = intent.getParcelableExtra(NOTIFICATION);
         int id = intent.getIntExtra(NOTIFICATION_ID, 0);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("notify_001",
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
+
         notificationManager.notify(id, notification);
 
     }
